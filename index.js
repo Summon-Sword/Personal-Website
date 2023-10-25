e09c0468bd57dedb337ba6312c1f526ea7088d1a
Summon-Sword <50055737+Summon-Sword@users.noreply.github.com>
2023-10-25 16:42:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+I'm resuing code from a previous assignment to fulfill the requirement for this assignment.
+*/
+let c1 = 0;
+let c2 = 0;
+let c3 = 0;
+
+// now do the opeartions to set the combo//
+c1 = c1 + 10;
+c2 = c2 + 50 - 10;
+c3 = c3 + 39 *1;
+
+const muser = "Hello world...";
+
+console.log(muser + c1 + c2 + c3);
+
+alert( muser );
+console.log(muser + c1 + c2 + c3);
